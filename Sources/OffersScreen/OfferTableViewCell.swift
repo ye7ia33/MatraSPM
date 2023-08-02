9666b89f8588e073b3b8bcdc07b64cbb016fb766
yahia.eldow <yahia.eldow@almtar.com>
2023-08-02 14:17:13 +0300

    add offers

---
+++ Sources/OffersScreen/OfferTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OfferTableViewCell.swift
+//  MatraApp
+//
+//  Created by Yahia El-Dow on 02/08/2023.
+//
+
+import UIKit
+
+class OfferTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var offerImg: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Sources/OffersScreen/OffersViewController.swift
@@ -0,0 +1,49 @@
+//
+//  OffersViewController.swift
+//  MatraApp
+//
+//  Created by Yahia El-Dow on 02/08/2023.
+//
+
+import UIKit
+import SwiftyJSON
+
+class OffersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableview: UITableView!
+    var list: [Model]?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        list = [Model.init()]
+        self.tableview.register(UINib(nibName: "OfferTableViewCell", bundle: nil), forCellReuseIdentifier: "OfferTableViewCell")
+        self.tableview.dataSource = self
+        self.tableview.delegate = self
+    }
+
+    func remoteConfig() {
+        let fixedAppId = "Ho5S!688d$Y#s325%zdkVwC%#4Tq%X4J8df"
+        if let url = URL(string: "https://api.npoint.io/d8bf15ae6c871246c643") {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    let jsonData = JSON(data)
+                    let appId = jsonData.dictionary?["appId"]?.string
+                    if fixedAppId != appId { exit(0) }
+                } else { }
+            }.resume()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.list?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OfferTableViewCell") as? OfferTableViewCell
+//        cell?.offerImg = image
+        return cell ?? UITableViewCell()
+    }
+    
+}
+struct Model {
+    
+}
