f0a2d208c2040a80c9bc04dc76cd6f029694e254
yahia.eldow <yahia.eldow@almtar.com>
2023-08-02 15:22:09 +0300

    .

--- Sources/MatraSPM/OffersScreen/OffersViewController.swift
+++ Sources/MatraSPM/OffersScreen/OffersViewController.swift
@@ -25,7 +25,7 @@ class OffersViewController: UIViewController, UITableViewDataSource, UITableView
         super.viewDidLoad()
         list = [Model.init()]
         self.view.backgroundColor = .red
-        let nib = UINib(nibName: "OfferTableViewCell", bundle: .module)
+        let nib = UINib(nibName: "OfferTableViewCell", bundle: .init(for: Self.self))
         self.tableview.register(nib, forCellReuseIdentifier: "OfferTableViewCell")
         self.tableview.dataSource = self
         self.tableview.delegate = self
